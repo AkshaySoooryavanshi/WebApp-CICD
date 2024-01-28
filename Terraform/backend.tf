65c5f0255f83543483af9a6f04fe44438af5f656
Akshay <96631562+AkshaySoooryavanshi@users.noreply.github.com>
2024-01-28 00:47:51 +0000

    Update backend.tf

--- Terraform/backend.tf
+++ Terraform/backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket         = "cicdawsbucket" # Created & Versioning Enabled Manually. made change
     key            = "terraform.tfstate"   # path and name of state file.
     region         = "eu-west-2"
-    dynamodb_table = "state_table" # name of dynamodb table for State Lock, must have partition key = "LockID"
+   # dynamodb_table = "state_table" # name of dynamodb table for State Lock, must have partition key = "LockID"
     # encrypt = true # by default
   }
 }
